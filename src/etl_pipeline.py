2a328c2f098d634832f16d4593ff906e63d7e46b
r3bcca13 <rebeccal5824@gmail.com>
2026-01-14 04:45:51 +1100

    Selected top 100 closest sites, summed volume by site ID and hour

--- src/etl_pipeline.py
+++ src/etl_pipeline.py
@@ -2,128 +2,87 @@ import io
 import logging
 from pathlib import Path
 import zipfile
-import pandas as pd
 import geopandas as gdp
+from geopy.distance import geodesic
+import pandas as pd
 
-# ================================================================================================================================
-# Logging
-# ================================================================================================================================
-
+# ---------------------------------------------------------------------------------------------------------------
+# LOGGING
+# ---------------------------------------------------------------------------------------------------------------
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s")
 
 logger = logging.getLogger(__name__)
 
-# ================================================================================================================================
-# Add suburb column
-# ================================================================================================================================
-
-# Read files
-sites_gdf = gdp.read_file("data/raw/traffic_site/traffic_site_metadata.geojson")
-suburbs = gdp.read_file("data/raw/traffic_site/suburbs.zip")
-suburbs = suburbs.to_crs(sites_gdf.crs)
-
-# Map a suburb to each site
-sites_gdf = gdp.sjoin(sites_gdf, 
-                      suburbs[["SAL_NAME21", "geometry"]], 
-                      how="left", 
-                      predicate="within")
-
-# Rename column names
-sites_gdf.rename(columns={
-                    "SITE_NO": "site_id",
-                    "SITE_NAME": "site_name",
-                    "SAL_NAME21": "suburb"}, 
-                 inplace=True)
-
-# Drop '(Vic. )' from end of suburb strings
-sites_gdf["suburb"] = sites_gdf["suburb"].str.replace(r"\s*\(Vic\.\)$", "", regex=True)
-
-# Sort table by suburb and site id
-sites_gdf.sort_values(by=["suburb", "site_id"], inplace=True)
-
-# Save processed file
-sites_gdf[["site_id", "site_name", "suburb"]].to_csv("data/processed/site_suburb.csv", index=False)
-
-# ================================================================================================================================
-# Process traffic volume files
-# ================================================================================================================================
-
+# ---------------------------------------------------------------------------------------------------------------
+# PATHS
+# ---------------------------------------------------------------------------------------------------------------
+SITE_GDF_FILE_PATH = Path("data/raw/traffic_site/traffic_site_metadata.geojson")
 DATA_DIR = Path("data/raw/traffic_volume")
-SITE_INFO_PATH = Path("data/processed/site_suburb.csv")
-OUTPUT_DIR = Path("data/processed")
+SITE_OUTPUT_DIR = Path("data/processed/traffic_site/top_100_sites.csv")
+VOLUME_OUTPUT_DIR = Path("data/processed/traffic_volume_v3")
 
-CSV_DTYPES = {
-    "NB_SCATS_SITE": "int16",
-    "NB_DETECTOR": "int8",
-    "CT_RECORDS": "int8"}
-
-SELECTED_SUBURBS = ["East Melbourne", "Richmond", "Cremorne", "Jolimont", "Melbourne", 
-                    "South Yarra", "Southbank", "South Melbourne", "Fitzroy", "Collingwood"]
-
-def map_suburb_2_site(site_info_path) -> pd.Series:
-    """ Load traffic site metadata and return selected site IDS. """
-
-    site_df = pd.read_csv(site_info_path)
-
-    selected_sites = (site_df.loc[site_df["suburb"].isin(SELECTED_SUBURBS), "site_id"]).astype("int16")
-
-    logger.info("Selected %d traffic sites", len(selected_sites))
-
-    return selected_sites
-
-def process_csv_file(csv_file: pd.DataFrame, selected_sites: list) -> pd.DataFrame:
+# ---------------------------------------------------------------------------------------------------------------
+# HELPER FUNCTIONS
+# ---------------------------------------------------------------------------------------------------------------
+def process_csv_file(csv_file: pd.DataFrame, selected_sites: pd.DataFrame) -> pd.DataFrame:
     """ Clean and transform a daily traffic volume CSV file. """
 
-    # Read file
-    df = pd.read_csv(csv_file, dtype=CSV_DTYPES)
+    # Read file and change datatypes
+    df = pd.read_csv(csv_file)
 
     # Drop irrelevant columns
-    df.drop(columns=["NM_REGION", "QT_VOLUME_24HOUR", "CT_ALARM_24HOUR"],
+    df.drop(columns=["NB_DETECTOR", 
+                     "NM_REGION", 
+                     "CT_RECORDS",
+                     "QT_VOLUME_24HOUR", 
+                     "CT_ALARM_24HOUR"], 
             inplace=True)
     
     # Rename columns
-    df.rename(columns={
-                "NB_SCATS_SITE": "site_id",
-                "QT_INTERVAL_COUNT": "datetime",
-                "NB_DETECTOR": "detector_id",
-                "CT_RECORDS": "working_period_count"},
+    df.rename(columns={"NB_SCATS_SITE": "site_id", 
+                       "QT_INTERVAL_COUNT": "date"}, 
               inplace=True)
     
-    # Filter suburbs of interest
-    df = df[df["site_id"].isin(selected_sites)]
+    # Filter top 100 closest sites
+    df = df[df["site_id"].isin(selected_sites["site_id"])]
 
+    # Replace NaN and negative volumes with zeros
     volume_cols = [c for c in df.columns if c.startswith("V")]
+    df = df.fillna(0)
+    df[volume_cols] = df[volume_cols].clip(lower=0)
 
     # Remove rows with no positive volumes
     df = df[(df[volume_cols] > 0).any(axis=1)]
 
-    # Replace NaN and negative volumes with zeros
-    df = df.fillna(0)
+    # Sum volume by site ID
+    volume_df = df[["site_id"] + volume_cols].groupby("site_id", as_index=False).sum()
+    date_df = df[["site_id", "date"]].drop_duplicates(subset="site_id")
+    df = pd.merge(volume_df, date_df, on="site_id", how="left")
+    df = df[["site_id", "date"] + volume_cols]
 
-    neg_mask = df[volume_cols] < 0
-    df["working_period_count"] -= neg_mask.sum(axis=1)
-    df[volume_cols] = df[volume_cols].mask(neg_mask, 0)
-
-    # Convert datatypes
-    df["datetime"] = pd.to_datetime(df["datetime"])
-    df[volume_cols] = df[volume_cols].astype("int16")
-
-    # Aggregate 15-minute volumes to hourly
-    hourly_vol = {f"{h+1}": df[volume_cols[h*4 : (h+1)*4]].sum(axis=1) for h in range(24)}
-    hourly_df = pd.DataFrame(hourly_vol, index=df.index)
-    df = pd.concat([df.drop(columns=volume_cols), hourly_df], axis=1)
+    # Sum 15-minute volumes by hour
+    volume_by_hour = {h+1: df[volume_cols[h*4 : (h+1)*4]].sum(axis=1) for h in range(24)}
+    volume_df = pd.DataFrame(volume_by_hour, index=df.index)
+    df = pd.concat([df.drop(columns=volume_cols), volume_df], axis=1)
+    volume_cols = volume_df.columns
 
     # Transform to long format
-    df = df.melt(id_vars=["site_id", "datetime", "detector_id", "working_period_count"],
-            value_vars=hourly_df.columns,
-            var_name="hour",
-            value_name="volume")
+    df = df.melt(id_vars=["site_id", "date"],
+                 value_vars=volume_cols,
+                 var_name="hour",
+                 value_name="volume")
     
-    return df[["datetime", "hour", "site_id", "detector_id", "volume", "working_period_count"]]
+        # Change datatypes
+    df["site_id"] = df["site_id"].astype("int16")
+    df["hour"] = df["hour"].astype("int8")
+    df["volume"] = df["volume"].astype("int16")
+    df["date"] = pd.to_datetime(df["date"]).dt.date
 
-def process_zip_file(zip_path: Path, selected_sites: list) -> pd.DataFrame:
+    return df[["date", "hour", "site_id", "volume"]]
+
+def process_zip_file(zip_path: Path, selected_sites: pd.DataFrame) -> pd.DataFrame:
     """ Process a yearly traffic volume ZIP file. """
 
     dfs = []
@@ -131,37 +90,53 @@ def process_zip_file(zip_path: Path, selected_sites: list) -> pd.DataFrame:
     with zipfile.ZipFile(zip_path) as parent_zip:
         for child_zip_name in sorted(parent_zip.namelist()):
             with parent_zip.open(child_zip_name) as child_zip:
-
-                child_bytes = io.BytesIO(child_zip.read())
-
-                with zipfile.ZipFile(child_bytes) as child_zip:
+                with zipfile.ZipFile(io.BytesIO(child_zip.read())) as child_zip:
                     for csv_name in sorted(child_zip.namelist()):
                         if csv_name.endswith(".csv"):
                             with child_zip.open(csv_name) as csv_file:
 
                                 df = process_csv_file(csv_file, selected_sites)
-                                logger.info("Processed %s", csv_name)
-
                                 dfs.append(df)
+                                logger.info("Processed %s", csv_name)
+                                break
+            break
 
     # Combine all CSVs
     return pd.concat(dfs, ignore_index=True)
 
-def main() -> None:
+# ---------------------------------------------------------------------------------------------------------------
+# SITE SELECTION
+# ---------------------------------------------------------------------------------------------------------------
+site_gdf = gdp.read_file(SITE_GDF_FILE_PATH)
+site_gdf.rename(columns={"SITE_NO": "site_id", "SITE_NAME": "site_name"}, inplace=True)
+site_gdf = site_gdf[["site_id", "site_name", "geometry"]]
+site_gdf = site_gdf.to_crs(epsg=4326)
 
-    selected_sites = map_suburb_2_site(SITE_INFO_PATH)
+# Calculate road distance between traffic site and destination of interest
+DESTINATION = (-37.8231, 144.9820) # Melbourne Park
+site_gdf["distance_to"] = site_gdf.geometry.apply(lambda geom: geodesic((geom.y, geom.x), DESTINATION).km)
+
+# Select top 100 closest traffic sites
+site_gdf.sort_values(by="distance_to", inplace=True)
+top_100_sites = site_gdf.iloc[:100, :3]
+top_100_sites.to_csv(SITE_OUTPUT_DIR)
+
+# ---------------------------------------------------------------------------------------------------------------
+# MAIN FUNCTION
+# ---------------------------------------------------------------------------------------------------------------
+def main() -> None:
 
     # Read each ZIP file in directory
     for zip_path in sorted(DATA_DIR.glob("*.zip")):
+
+        df = process_zip_file(zip_path, top_100_sites)
         
         year = zip_path.stem[-4:]
-
-        df = process_zip_file(zip_path, selected_sites)
-
-        output_path = OUTPUT_DIR / f"traffic_volume_{year}.parquet"
+        output_path = VOLUME_OUTPUT_DIR / f"traffic_volume_{year}.parquet"
 
         # Saved processed yearly file
-        df.to_parquet(output_path, index=False)
+        df.to_parquet(output_path,
+                      index=False)
         logger.info("Saved %s", output_path)
 
 if __name__ == "__main__":
